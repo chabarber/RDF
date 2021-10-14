c3948ad68ce468aa422b7b04965382fa2fe08a6c
Chase <chabarber@ksu.edu>
2021-10-14 16:48:55 -0500

    initial commit

---
+++ RDF/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace RDF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+       
+
+       
+        public static void CheckFileExists(string filename)
+        {
+            if(!System.IO.File.Exists(filename))
+            {
+                Console.WriteLine("File does not exist");
+                //return true;       
+            }
+        }
+        
+    }
+
+    
+}
